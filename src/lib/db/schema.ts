fc6a53bd799bfef15615f4e1d0fcea92cb088cc5
Brian Morrison II <brian@brianmorrison.me>
2023-05-13 22:20:12 -0400

    add api, wire up user page

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -7,5 +7,20 @@ dotenv.config()
 export const blocks = mysqlTable('blocks', {
   id: serial('id').primaryKey(),
   url: varchar('url', { length: 200 }),
-  block_type: int('type')
+  block_type: int('type'),
+  user_id: int('user_id')
 });
+
+export const sections = mysqlTable('sections', {
+  id: serial('id').primaryKey(),
+  user_id: int('user_id'),
+  size: int('size'), 
+  display_order: int('display_order')
+})
+
+export const users = mysqlTable('users', {
+  id: serial('id').primaryKey(),
+  username: varchar('username', { length: 120 }),
+  tagline: varchar('tagline', { length: 250 }),
+  display_name: varchar('display_name', { length: 250 }),
+})
