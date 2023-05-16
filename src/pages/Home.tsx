a752d4439c2bf1925bdcdcb3d034cac3a3f8cadf
Brian Morrison II <brian@planetscale.com>
2023-05-15 22:45:51 -0400

    fix home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import ProfileSummaryCard from '../components/ProfileSummaryCard'
 
 function Home() {
   const navigate = useNavigate()
-  const [profiles, setProfiles] = useState<any>()
+  const [profiles, setProfiles] = useState<Profile[]>([])
 
   useEffect(() => {
     // TODO: Loading state
@@ -21,8 +21,8 @@ function Home() {
     <div className="flex flex-col">
       {/* Hero */}
       <div className="h-[80vh] flex flex-col gap-2 justify-center items-center w-screen">
-        <div className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 to-slate-500">Out of this world</div>
-        <div className="text-6xl font-bold mb-4 shadow-sm">bio-link service</div>
+        <div className="text-center text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 to-slate-500">Out of this world</div>
+        <div className="text-center text-6xl font-bold mb-4">bio-link service</div>
         <button className="rounded-xl p-3 font-bold bg-slate-800 text-slate-50 hover:shadow-lg hover:bg-slate-700"
           onClick={() => navigate("/profile")}>Create your profile!</button>
       </div>
