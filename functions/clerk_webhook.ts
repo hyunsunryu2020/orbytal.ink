db3b2b431474ee780e03268543a3d07438ddd3d6
Brian Morrison II <brian@planetscale.com>
2023-05-20 20:56:59 -0400

    update nav and webhook

--- functions/clerk_webhook.ts
+++ functions/clerk_webhook.ts
@@ -1,10 +1,8 @@
 import { HandlerEvent, HandlerContext } from "@netlify/functions";
-import clerk from '@clerk/clerk-sdk-node';
-import { getPageBlocksForUsername } from "./utils/lib";
 
 
 const handler = async (event: HandlerEvent, context: HandlerContext) => {
-  console.log(JSON.stringify(event))
+  console.log(event.body)
 
   // your server-side functionality
   return {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,24 @@
-import { UserButton } from '@clerk/clerk-react'
+import { UserButton, useUser } from '@clerk/clerk-react'
+import { useNavigate } from 'react-router-dom';
+import { FaPencilAlt } from 'react-icons/fa';
 
 function Nav() {
+  const navigate = useNavigate()
+  const { isSignedIn, isLoaded } = useUser();
+
   return (
     <div className='flex justify-end p-4 gap-2'>
-      {/* {isSignedIn && 
-        <button className="px-2 hover:bg-slate-300 hover:shadow-sm transition-all rounded-xl"
-          onClick={() => navigate("/profile")}><FaPencilAlt /></button>
-      } */}
+      {/* { isLoaded && (
+        <>
+          {isSignedIn ?
+            <button className="px-2 hover:bg-slate-300 hover:shadow-sm transition-all rounded-xl"
+              onClick={() => navigate("/profile")}><FaPencilAlt /></button>
+            :
+            <button className="py-2 px-3 hover:bg-slate-300 hover:text-slate-900 hover:shadow-sm transition-all rounded-xl"
+              onClick={() => navigate("/sign-in")}>Sign in</button>
+          }
+        </>
+      )} */}
       <UserButton />
     </div>
   )
