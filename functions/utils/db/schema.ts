520565b05327589ce294197e5b652802f2b10c9b
Ryu Hyunsun <101242965+hyunsunryu2020@users.noreply.github.com>
2024-02-12 16:03:19 +0000

    Update schema.ts

--- functions/utils/db/schema.ts
+++ functions/utils/db/schema.ts
@@ -2,35 +2,34 @@ import { relations } from "drizzle-orm";
 import { mysqlTable, serial, varchar, int } from "drizzle-orm/mysql-core";
 
 export const users = mysqlTable('users', {
-  id: serial('id').primaryKey(),
-  username: varchar('username', { length: 120 }),
-  tagline: varchar('tagline', { length: 250 }),
-  display_name: varchar('display_name', { length: 250 }),
-  img_url: varchar('img_url', { length: 500 }),
+  user_id: serial('user_id').primaryKey(),
+  email: varchar('email', { length: 120 }).notNull,
+  password_hash: varchar('password_hash', { length: 250 }).notNull,
+  created_at: timestamp('created_at').defaultNow().notNull
 })
 
-export const usersRelations = relations(users, ({ many }) => ({
-  blocks: many(blocks)
-}));
+// export const usersRelations = relations(users, ({ many }) => ({
+//   blocks: many(blocks)
+// }));
 
-export const blocks = mysqlTable('blocks', {
-  id: serial('id').primaryKey(),
-  url: varchar('url', { length: 200 }),
-  block_type: int('type'),
-  user_id: int('user_id'),
-  label: varchar('label', { length: 200 }),
-});
+// export const blocks = mysqlTable('blocks', {
+//   id: serial('id').primaryKey(),
+//   url: varchar('url', { length: 200 }),
+//   block_type: int('type'),
+//   user_id: int('user_id'),
+//   label: varchar('label', { length: 200 }),
+// });
 
-export const blocksRelations = relations(blocks, ({ one }) => ({
-  user: one(users, {
-    fields: [blocks.user_id],
-    references: [users.id]
-  })
-}))
+// export const blocksRelations = relations(blocks, ({ one }) => ({
+//   user: one(users, {
+//     fields: [blocks.user_id],
+//     references: [users.id]
+//   })
+// }))
 
-export const sections = mysqlTable('sections', {
-  id: serial('id').primaryKey(),
-  user_id: int('user_id'),
-  size: int('size'), 
-  display_order: int('display_order')
-})
+// export const sections = mysqlTable('sections', {
+//   id: serial('id').primaryKey(),
+//   user_id: int('user_id'),
+//   size: int('size'), 
+//   display_order: int('display_order')
+// })
