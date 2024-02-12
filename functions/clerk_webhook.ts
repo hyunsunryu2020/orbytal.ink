530da9b777dc441f166c5d46186908d908d4edbf
Ryu Hyunsun <101242965+hyunsunryu2020@users.noreply.github.com>
2024-02-12 22:32:28 +0800

    Create clerk_webhook.ts

---
+++ functions/clerk_webhook.ts
@@ -0,0 +1,69 @@
+// functions/clerk_webhook.ts
+import { HandlerEvent, HandlerContext } from '@netlify/functions'
+import { getDb } from './utils/lib'
+import { blocks, users } from './utils/db/schema'
+import { eq } from 'drizzle-orm'
+// This type describes the structure of the incoming webhook
+type ClerkWebhook = {
+  data: {
+    first_name: string
+    last_name: string
+    image_url: string
+    username: string
+  }
+  type: string
+}
+const handler = async (event: HandlerEvent, context: HandlerContext) => {
+  if (event.body) {
+    // 👉 Parse the incomign event body into a ClerkWebhook object
+    const webhook = JSON.parse(event.body) as ClerkWebhook
+    try {
+      const db = getDb()
+      // 👉 `webhook.type` is a string value that describes what kind of event we need to handle
+
+      // 👉 If the type is "user.updated" the important values in the database will be updated in the users table
+      if (webhook.type === 'user.updated') {
+        await db
+          .update(users)
+          .set({
+            display_name: `${webhook.data.first_name} ${webhook.data.last_name}`,
+            img_url: webhook.data.image_url
+          })
+          .where(eq(users.username, webhook.data.username))
+      }
+
+      // 👉 If the type is "user.created" create a record in the users table
+      if (webhook.type === 'user.created') {
+        await db.insert(users).values({
+          display_name: `${webhook.data.first_name} ${webhook.data.last_name}`,
+          img_url: webhook.data.image_url,
+          username: webhook.data.username
+        })
+      }
+
+      // 👉 If the type is "user.deleted", delete the user record and associated blocks
+      if (webhook.type === 'user.deleted') {
+        const dbuser = await db.query.users.findFirst({
+          where: eq(users.username, webhook.data.username)
+        })
+        console.log('dbuser', dbuser)
+        if (dbuser) {
+          await Promise.all([
+            db.delete(users).where(eq(users.id, dbuser.id)),
+            db.delete(blocks).where(eq(blocks.user_id, dbuser.id))
+          ])
+        }
+      }
+
+      return {
+        statusCode: 200
+      }
+    } catch (err) {
+      console.error(err)
+      return {
+        statusCode: 500
+      }
+    }
+  }
+}
+export { handler }
